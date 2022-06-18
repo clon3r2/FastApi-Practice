9abfb10af6d8a308d4f182eeb0d1ff37dd43dab3
alireza <alirezac257@gmail.com>
2022-06-19 00:59:58 +0430

    implement delete and retrieve routes

--- server/routes/student.py
+++ server/routes/student.py
@@ -13,22 +13,32 @@ from server.models.student import (
     ResponseModel,
     StudentSchema,
     UpdateStudentModel,
+    Tags,
 )
 
 router = APIRouter()
 
-@router.post("/create", response_description="Student data added into the database")
+
+@router.post("/create", response_description="Student data added into the database", tags=[Tags.student])
 async def add_student_data(student: StudentSchema = Body(...)):
     student = jsonable_encoder(student)
     new_student = await add_student(student)
     return ResponseModel(new_student, "Student added successfully.")
 
 
-@router.put('/update/{student_id}')
-async def update_student_data(student_id: str, student: UpdateStudentModel= Body(...)):
-    print('student: ', student)
-    student = jsonable_encoder(student)
-    print('encoded student: ', student)
+@router.put("/update/{student_id}")
+async def update_student_data(student_id: str, student: UpdateStudentModel = Body(...), tags=[Tags.student]):
+    student = jsonable_encoder(student.dict())
     new_student = await update_student(student_id, student)
-    print('new student: ', new_student)
     return ResponseModel(new_student, "Student Updated Successfuly")
+
+@router.delete('/delete/{student_id}', tags=[Tags.student])
+async def delete_student_data(student_id: str):
+    await delete_student(student_id)
+    return {'message': "Student Deleted Successfuly."}
+
+
+@router.get('/retreive/{student_id}', status_code=200)
+async def retreive_student_data(student_id: str):
+    student = await retrieve_student(student_id)
+    return ResponseModel(student, 'Fetched Requested Student Successfuly.')
