23c0dba42d142247cbc5f1dec4d0ccfee0ec6d39
alireza <alirezac257@gmail.com>
2022-06-19 00:59:06 +0430

    refactored update student to partial update

--- server/database.py
+++ server/database.py
@@ -53,15 +53,10 @@ async def update_student(id: str, data: dict):
     # Return false if an empty request body is sent.
     if len(data) < 1:
         return False
-    cleaned_data: dict = {}
-    for key, value in data.items():
-        if data[key] is not None:
-            cleaned_data[key] = value
-    print(cleaned_data)
     student = await student_collection.find_one({"_id": ObjectId(id)})
     if student:
         updated_student = await student_collection.update_one(
-            {"_id": ObjectId(id)}, {"$set": cleaned_data}
+            {"_id": ObjectId(id)}, {"$set": data}
         )
         if updated_student:
             return True
